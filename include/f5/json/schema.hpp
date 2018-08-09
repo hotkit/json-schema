0910aa434874f9a5366d112df9a6cb3a9c30620c
Kirit Sælensminde <k@kirit.com>
2018-08-09 10:57:31 +0700

    Add schema header.

---
+++ include/f5/json/schema.hpp
@@ -0,0 +1,28 @@
+/**
+    Copyright 2018, Proteus Technologies Co Ltd. <https://support.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+
+#pragma once
+
+
+namespace f5 {
+
+
+    inline namespace js {
+
+
+        namespace json_schema {
+
+
+        }
+
+
+    }
+
+
+}
+
