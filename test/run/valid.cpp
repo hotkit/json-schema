13b1b147d89de7b5f19d11d820518bf00f66da38
Kirit Sælensminde <k@kirit.com>
2018-08-09 11:55:44 +0700

    Start to lay out how the tests need to look.

--- test/run/valid.cpp
+++ test/run/valid.cpp
@@ -7,8 +7,38 @@
 
 #include <f5/json/validator.hpp>
 
+#include <fost/file>
+#include <fost/main>
+#include <fost/unicode>
 
-int main() {
+
+namespace {
+    const fostlib::setting<bool> c_check_invalid(__FILE__,
+        "json-schema-validator", "Success when invalid", false, true);
+
+    const fostlib::setting<fostlib::string> c_schema(__FILE__,
+        "json-schema-validator", "Schema file name", "any.schema.json", true);
+
+//     auto load_json(f5::u8view fn) {
+//         return fostlib::json::parse(
+//             fostlib::utf::load_file(fostlib::coerce<boost::filesystem::path>(fn)));
+//     }
+}
+
+
+FSL_MAIN(
+    "json-schema-validator",
+    "JSON Schema Validator"
+) (fostlib::ostream &out, fostlib::arguments &args) {
+    args.commandSwitch("i", c_check_invalid);
+    args.commandSwitch("-schema", c_schema);
+
+    const auto schema = fostlib::json::parse(
+        fostlib::utf::load_file(fostlib::coerce<boost::filesystem::path>(c_schema.value())));
+
+//     for ( const auto &&arg : args ) {
+//     }
+
     return 0;
 }
 
