4d0bb7d1afb3cbbdbfb5f943b4b92b75f37f41b3
Kirit Sælensminde <k@kirit.com>
2018-08-09 11:12:19 +0700

    Will need to be able to run schemas against JSON to test the validation is correct.

---
+++ test/run/valid.cpp
@@ -0,0 +1,14 @@
+/**
+    Copyright 2018, Proteus Technologies Co Ltd. <https://support.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+#include <f5/json/validator.hpp>
+
+
+int main() {
+    return 0;
+}
+
