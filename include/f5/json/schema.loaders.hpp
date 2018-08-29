0308ec0490556bff5e17e61abacc88c63fd1ca1a
Kirit Sælensminde <k@kirit.com>
2018-08-29 12:52:36 +0700

    Add schema loaders header.

---
+++ include/f5/json/schema.loaders.hpp
@@ -0,0 +1,24 @@
+/**
+    Copyright 2018, Proteus Technologies Co Ltd. <https://support.felspar.com/>
+
+    Distributed under the Boost Software License, Version 1.0.
+    See <http://www.boost.org/LICENSE_1_0.txt>
+*/
+
+
+#pragma once
+
+#include <f5/json/schema.hpp>
+
+
+namespace f5 {
+
+
+    namespace json {
+
+
+    }
+
+
+}
+
